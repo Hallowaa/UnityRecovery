136b9607e8ea70a3ad4e660a8711e24c290426b4
Hallowa <d4n1el1t0mk@gmail.com>
2020-01-17 19:58:41 +0100

    First Commit

---
+++ 2dplatformer/Assets/scripts/Player.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+
+    public float jumpHeight = 3.5f;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .05f;
+    float accelerationTimeGrounded = .015f;
+    float moveSpeed = 19f;
+
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+    float timeToWallUnstick;
+    public bool canSlide;
+
+    public float dashDistance;
+    public float dashTime;
+    float startDashTime = .1f;
+    float elapsedTime = Mathf.Infinity;
+    public bool dash;
+
+    float gravity;
+    float jumpVelocity;
+    Vector2 velocity;
+    float velocityXSmoothing;
+    [HideInInspector]
+    public Controller2D controller;
+
+    Vector2 directionalInput;
+    bool wallSliding;
+    int wallDirX;
+    
+    private void Start()
+    {
+        controller = GetComponent<Controller2D>();
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity * timeToJumpApex);
+        print("Gravity: " + gravity + "   Jump Velocity:   " + jumpVelocity);
+
+        dashTime = startDashTime;
+    }
+
+    private void FixedUpdate()
+    {
+        
+        CalculateVelocity();
+        HandleWallSliding();
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            if (controller.collisions.slidingDownMaxSlope)
+            {
+                velocity.y += controller.collisions.slopeNormal.y * -gravity * Time.deltaTime;
+
+            }
+            else
+            {
+                velocity.y = 0;
+            }
+        }
+
+        if (dash)
+        {
+            elapsedTime = 0f;
+        }
+
+        if (elapsedTime < dashTime)
+        {           
+            elapsedTime += Time.deltaTime;
+            gravity = 0;
+            controller.collisions.below = false; 
+            OnDashInputDown();
+        }
+        else if (elapsedTime > dashTime)
+        {
+            gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2); 
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            dash = true;
+        }       
+    }
+
+    public void SetDirectionalInput (Vector2 input)
+    {
+        directionalInput = input;
+    }
+
+    public void OnJumpInputdown()
+    {
+
+        if (wallSliding)
+        {
+            if (wallDirX == directionalInput.x)
+            {
+                velocity.x = -wallDirX * wallJumpClimb.x;
+                velocity.y = wallJumpClimb.y;
+            }
+            else if (directionalInput.x == 0)
+            {
+                velocity.x = -wallDirX * wallJumpOff.x;
+                velocity.y = wallJumpOff.y;
+            }
+            else
+            {
+                velocity.x = -wallDirX * wallLeap.x;
+                velocity.y = wallLeap.y;
+            }
+        }
+
+        if (controller.collisions.below)
+        {
+            if (controller.collisions.slidingDownMaxSlope)
+            {
+                if (directionalInput.x != -Mathf.Sign(controller.collisions.slopeNormal.x)) // not jumping against max slope
+                {
+                    velocity.y = jumpVelocity * controller.collisions.slopeNormal.y;
+                    velocity.x = jumpVelocity * controller.collisions.slopeNormal.x;
+                }
+                
+            }
+            velocity.y = jumpVelocity;
+        }
+
+    }
+
+    public void OnDashInputDown()
+    {   
+         if (dash && controller.collisions.faceDir == 1)
+         {
+             controller.collisions.below = false;
+             velocity.y = 0;
+             velocity = Vector2.right * dashDistance;
+             dash = false;
+         }
+         else if (dash && controller.collisions.faceDir == -1)
+         {
+             controller.collisions.below = false;
+             velocity.y = 0;
+             velocity = Vector2.left * dashDistance;
+             dash = false;
+         }       
+    } 
+
+    void HandleWallSliding()
+    {
+        
+        wallDirX = (controller.collisions.left) ? -1 : 1;
+        wallSliding = false;
+        if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0 && canSlide)
+        {
+                wallSliding = true;
+
+                if (velocity.y < -wallSlideSpeedMax)
+                {
+                    velocity.y = -wallSlideSpeedMax;
+                }
+
+                if (timeToWallUnstick > 0)
+                {
+                    velocityXSmoothing = 0;
+                    velocity.x = 0;
+
+                    if (directionalInput.x != wallDirX && directionalInput.x != 0)
+                    {
+                        timeToWallUnstick -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        timeToWallUnstick = wallStickTime;
+                    }
+                }
+                else
+                {
+                    timeToWallUnstick = wallStickTime;
+                }
+        }
+    }
+
+    void CalculateVelocity()
+    {
+        float targetVelocityX = directionalInput.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+}
+
+
---
+++ 2dplatformer/Assets/scripts/RaycastController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour
+{
+
+    public LayerMask collisionMask;
+
+    public const float skinWidth = .015f;
+    const float dstBetweenRays = .4f;
+    [HideInInspector]
+    public int horizontalRayCount;
+    [HideInInspector]
+    public int verticalRayCount;
+
+    [HideInInspector]
+    public float horizontalRaySpacing;
+    [HideInInspector]
+    public float verticalRaySpacing;
+    [HideInInspector]
+    public new BoxCollider2D collider;
+    [HideInInspector]
+    public RaycastOrigins raycastOrigins;
+
+    public virtual void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    public void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomleft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomright = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topleft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topright = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    public void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        float boundsWidth = bounds.size.x;
+        float boundsHeight = bounds.size.y;
+
+        horizontalRayCount = Mathf.RoundToInt(boundsHeight / dstBetweenRays);
+        verticalRayCount = Mathf.RoundToInt(boundsWidth / dstBetweenRays);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    public struct RaycastOrigins
+    {
+        public Vector2 topleft, topright;
+        public Vector2 bottomleft, bottomright;
+    }
+}
